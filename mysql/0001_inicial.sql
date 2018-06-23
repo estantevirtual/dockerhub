d182a06d8bbb491b8180cdaf3ce59babf6e30d85
Ezequiel Bertti <ebertti@yahoo.com.br>
2018-06-23 13:29:30 -0300

    Delete 0001_inicial.sql

--- mysql/0001_inicial.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE marketing CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'marketing'@'%' IDENTIFIED BY 'marketing';
-GRANT ALL PRIVILEGES ON *.* to 'marketing'@'%';
-FLUSH PRIVILEGES;
